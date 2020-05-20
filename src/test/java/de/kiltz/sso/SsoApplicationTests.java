db49b3824ffba15b3111dee2db30f30f9aef2e6f
Friedrich <f@kiltz.de>
2020-05-20 13:38:47 +0200

    mit Selenium-Tests

--- src/test/java/de/kiltz/sso/SsoApplicationTests.java
+++ src/test/java/de/kiltz/sso/SsoApplicationTests.java
@@ -1,13 +1,21 @@
 package de.kiltz.sso;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import de.kiltz.sso.service.KontoService;
 
 @SpringBootTest
 class SsoApplicationTests {
 
+	@Autowired
+	private KontoService service;
 	@Test
 	void contextLoads() {
+		assertNotNull(service);
 	}
 
 }
