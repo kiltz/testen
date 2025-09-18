4bc180946508ba52a4b6609711e06289ee89ea8e
Igor <igor.pyo@gmail.com>
2025-09-18 09:55:28 +0200

    testFindByEmail

--- src/test/java/de/kiltz/sso/dao/KontoCRUDTest.java
+++ src/test/java/de/kiltz/sso/dao/KontoCRUDTest.java
@@ -3,20 +3,28 @@ package de.kiltz.sso.dao;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import de.kiltz.sso.data.KontoEntity;
+import org.springframework.test.context.jdbc.Sql;
 
 /**
  * @author tz
  */
 @SpringBootTest
+@Sql(scripts = "/dataCrud.sql")
 class KontoCRUDTest {
 
     @Autowired
     private KontoDao dao;
+
+    @AfterEach
+    void afterEach() {
+        dao.deleteAll();
+    }
 
     @Test
     void testInsert(){
@@ -29,5 +37,11 @@ class KontoCRUDTest {
 
         KontoEntity e2 = dao.save(e);
         assertNotNull(e2.getId());
+    }
+
+    @Test
+    void testFindByEmail(){
+        KontoEntity ke = dao.findByEmail("test2@testa.de");
+        assertNotNull(ke);
     }
 }
