13466715bf737ee2be5d467f42fa76f13f8e95ed
Igor <igor.pyo@gmail.com>
2025-09-18 15:24:03 +0200

    Add EmailValidatorTest.java

---
+++ src/test/java/de/kiltz/sso/utils/EmailValidatorTest.java
@@ -0,0 +1,46 @@
+package de.kiltz.sso.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EmailValidatorTest {
+
+    private static List<String> getTestCorrectEmails() {
+        return List.of(
+                "POSTfACH@meta.ua",
+                "weB3t@gmail.com",
+                "999lucky@hotmail.com",
+                "Dylan@use.net",
+                "S.dorn@web.de"
+        );
+    }
+
+    private static List<String> getTestInCorrectEmails() {
+        return List.of(
+                "",
+                "@",
+                "weB&t@gmail.com",
+                "@hotmail.com",
+                "Dylan@.net",
+                "S.dorn@web."
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTestCorrectEmails")
+    void testValidEmail(String email) {
+        assertTrue(TextUtils.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTestInCorrectEmails")
+    void testInvalidEmail(String email) {
+        assertFalse(TextUtils.validateEmail(email));
+    }
+
+}
