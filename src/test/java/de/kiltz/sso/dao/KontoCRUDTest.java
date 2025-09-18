d218a6fd8bbd86f75b734d5dcf62f0f7a57f42e8
Igor <igor.pyo@gmail.com>
2025-09-18 11:32:21 +0200

    KontoCRUDTest.java is ready

--- src/test/java/de/kiltz/sso/dao/KontoCRUDTest.java
+++ src/test/java/de/kiltz/sso/dao/KontoCRUDTest.java
@@ -1,8 +1,5 @@
 package de.kiltz.sso.dao;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import de.kiltz.sso.data.KontoEntity;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author tz
  */
 @SpringBootTest
-@Sql(scripts = "/dataCrud.sql")
+@Sql(scripts = "/dataCrud.sql",  executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Transactional
 class KontoCRUDTest {
+
+    public static final String DEFAULT_EMAIL = "test@testa.de";
 
     @Autowired
     private KontoDao dao;
@@ -26,10 +31,11 @@ class KontoCRUDTest {
         dao.deleteAll();
     }
 
+    //region CREATE
     @Test
     void testInsert(){
         KontoEntity e = new KontoEntity();
-        e.setEmail("test@testa.de");
+        e.setEmail(DEFAULT_EMAIL);
         e.setNachname("Rossa");
         e.setVorname("Testa");
         e.setPasswort("keins");
@@ -38,10 +44,71 @@ class KontoCRUDTest {
         KontoEntity e2 = dao.save(e);
         assertNotNull(e2.getId());
     }
+    //endregion CREATE
 
+    //region READ
+    @Test
+    void testFindById() {
+        KontoEntity kontoUngespeicherrt = getDefaultKonto();
+        KontoEntity kontoGespeicherrt = dao.save(kontoUngespeicherrt);
+
+        assertNotNull(kontoGespeicherrt.getId());
+
+        KontoEntity kontoByID = dao.getReferenceById(kontoGespeicherrt.getId());
+        assertNotNull(kontoByID);
+    }
+
+    private KontoEntity getDefaultKonto() {
+        KontoEntity e = new KontoEntity();
+        e.setNachname("Rossa");
+        e.setVorname("Testa");
+        e.setPasswort("keins");
+        e.setEmail(DEFAULT_EMAIL);
+        return e;
+    }
+
+    /**
+     * Sucht nach der E-Mail aus dataCrud.sql
+     */
     @Test
     void testFindByEmail(){
         KontoEntity ke = dao.findByEmail("test2@testa.de");
         assertNotNull(ke);
     }
+
+    @Test
+    void testFindByNachname() {
+        List<KontoEntity> lke = dao.findByNachnameContains("Rossa");
+        KontoEntity ke = lke.getFirst();
+        assertNotNull(ke);
+    }
+
+    @Test
+    void testFindByPasswortAndEmail() {
+        KontoEntity ke = dao.findByEmailAndPasswort(DEFAULT_EMAIL, "keins");
+        assertNotNull(ke);
+        assertEquals(DEFAULT_EMAIL, ke.getEmail());
+        assertEquals("keins", ke.getPasswort());
+    }
+    //endregion READ
+
+    //region UPDATE&DELETE
+
+    @Test
+    void testUpdateAndDelete() {
+        KontoEntity e = getDefaultKonto();
+        KontoEntity e2 = dao.save(e);
+        assertNotNull(e.getPasswort(), e2.getPasswort());
+
+        e2.setPasswort("neuesPasswort");
+        dao.save(e2);
+
+        KontoEntity kontoGeholtPerID = dao.getReferenceById(e2.getId());
+        assertEquals("neuesPasswort", kontoGeholtPerID.getPasswort());
+
+        dao.delete(kontoGeholtPerID);
+        var erg = dao.findByEmail(e2.getEmail());
+        assertNull(erg);
+    }
+    //endregion UPDATE&DELETE
 }
