0254df8067104817e8b52f5a5dc7128a7101c2ff
Friedrich Kiltz <f@kiltz.de>
2025-09-18 11:32:12 +0200

    CRUD-Test fertig

--- src/test/java/de/kiltz/sso/dao/KontoCRUDTest.java
+++ src/test/java/de/kiltz/sso/dao/KontoCRUDTest.java
@@ -1,5 +1,6 @@
 package de.kiltz.sso.dao;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,16 +33,27 @@ class KontoCRUDTest {
 
     }
 
+    @Test
+    void testAendernUndLoeschen() {
+        String neuesPaswwort = "neuesPasswort";
 
-    /*
-     TODO: Test für
-     * Lesen
-        - per id dao.getReferenceById()
-        - über die 3 SuchMethoden findBy...
-     * Ändern dao.save(..)
-     * Löschen dao.delete(..)
+        KontoEntity e = getDefaultKonto();
+        KontoEntity e2 = dao.save(e);
+        assertEquals(e.getPasswort(), e2.getPasswort());
 
-    */
+        e2.setPasswort(neuesPaswwort);
+
+        dao.save(e2);
+
+        KontoEntity kontoGeholtPerId = dao.getReferenceById(e2.getId());
+        assertEquals(neuesPaswwort, kontoGeholtPerId.getPasswort());
+
+        dao.delete(kontoGeholtPerId);
+        var erg = dao.findByEmail(e2.getEmail());
+        assertNull(erg);
+
+    }
+
 
     @Test
     void testLogin() {
@@ -50,6 +62,23 @@ class KontoCRUDTest {
 
         KontoEntity nichtGefunden = dao.findByEmailAndPasswort(DEFAULT_MAIL, "falsch");
         assertNull(nichtGefunden);
+    }
+    @Test
+    void testFindByEmail() {
+        KontoEntity gefunden = dao.findByEmail(DEFAULT_MAIL);
+        assertEquals(DEFAULT_MAIL, gefunden.getEmail());
+
+        KontoEntity nichtGefunden = dao.findByEmail(DEFAULT_MAIL+"nichtDa");
+        assertNull(nichtGefunden);
+    }
+
+    @Test
+    @Sql(scripts = "/dataCrudFindByNachname.sql")
+    void testFindByNachname() {
+        var ergebnis = dao.findByNachnameContainsIgnoreCase("c");
+        assertEquals(2, ergebnis.size());
+        ergebnis = dao.findByNachnameContainsIgnoreCase("a");
+        assertEquals(1, ergebnis.size());
     }
 
     @Test
