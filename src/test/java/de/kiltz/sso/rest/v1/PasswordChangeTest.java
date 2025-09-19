e9f05e0013bb1bb726aae442dda23f767ae0b4c3
Philip <test@web.de>
2025-09-19 12:31:57 +0200

    Verbessern der tests um passwortsetzen zu prüfen

--- src/test/java/de/kiltz/sso/rest/v1/PasswordChangeTest.java
+++ src/test/java/de/kiltz/sso/rest/v1/PasswordChangeTest.java
@@ -1,5 +1,6 @@
 package de.kiltz.sso.rest.v1;
 
+import de.kiltz.sso.service.KontoService;
 import de.kiltz.sso.service.SSOValidationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 @Sql(scripts = "/dataPasswordChange.sql")
@@ -21,29 +23,36 @@ class PasswordChangeTest {
     private static final String NEW_PASSWORD = "Deins&Se1ns!";
 
     @Autowired
-    KontoRestService service;
+    KontoRestService kontoRestService;
+    @Autowired
+    KontoService kontoService;
 
     @Test
     void testChangePasswordGoodDay() throws SSOValidationException {
-        assertEquals(new ResponseEntity<>(HttpStatus.NO_CONTENT), service.changePassword(DEFAULT_EMAIL, DEFAULT_PASSWORD, NEW_PASSWORD));
+        assertEquals(new ResponseEntity<>(HttpStatus.NO_CONTENT), kontoRestService.changePassword(DEFAULT_EMAIL, DEFAULT_PASSWORD, NEW_PASSWORD));
 
+        assertEquals(NEW_PASSWORD, kontoService.holePerEmail(DEFAULT_EMAIL).getPasswort());
         //cleanup for next Test
-        service.changePassword(DEFAULT_EMAIL, NEW_PASSWORD, DEFAULT_PASSWORD);
+        kontoRestService.changePassword(DEFAULT_EMAIL, NEW_PASSWORD, DEFAULT_PASSWORD);
     }
 
     @Test
     void testIncorrectOldPassword() throws SSOValidationException {
-        assertEquals(new ResponseEntity<>(HttpStatus.FORBIDDEN), service.changePassword(DEFAULT_EMAIL, NEW_PASSWORD, NEW_PASSWORD));
+        assertEquals(new ResponseEntity<>(HttpStatus.FORBIDDEN), kontoRestService.changePassword(DEFAULT_EMAIL, NEW_PASSWORD, NEW_PASSWORD));
+
+        assertNotEquals(NEW_PASSWORD, kontoService.holePerEmail(DEFAULT_EMAIL).getPasswort());
     }
 
     @Test
     void testInvalidNewPassword() throws SSOValidationException {
-        assertEquals(new ResponseEntity<>(HttpStatus.BAD_REQUEST), service.changePassword(DEFAULT_EMAIL, DEFAULT_PASSWORD, "keins"));
+        assertEquals(new ResponseEntity<>(HttpStatus.BAD_REQUEST), kontoRestService.changePassword(DEFAULT_EMAIL, DEFAULT_PASSWORD, "keins"));
+        assertNotEquals("keins", kontoService.holePerEmail(DEFAULT_EMAIL).getPasswort());
     }
 
     @Test
     void testInvalidEmail() throws SSOValidationException {
-        assertEquals(new ResponseEntity<>(HttpStatus.FORBIDDEN), service.changePassword("bad@email.org", NEW_PASSWORD, NEW_PASSWORD));
+        assertEquals(new ResponseEntity<>(HttpStatus.FORBIDDEN), kontoRestService.changePassword("bad@email.org", NEW_PASSWORD, NEW_PASSWORD));
+        assertNotEquals(NEW_PASSWORD, kontoService.holePerEmail(DEFAULT_EMAIL).getPasswort());
     }
 }
 
