f1a62e0afe04074054cb30d75408ee712a3287c6
Igor <igor.pyo@gmail.com>
2025-09-19 11:13:00 +0200

    Speicherung

--- src/test/java/de/kiltz/sso/rest/v1/LoginRestServiceTest.java
+++ src/test/java/de/kiltz/sso/rest/v1/LoginRestServiceTest.java
@@ -1,4 +1,14 @@
 package de.kiltz.sso.rest.v1;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class LoginRestServiceTest {
+
+    @Test
+    void loginTest()
+    {
+    }
+
 }
--- src/test/java/de/kiltz/sso/service/SsoServiceTest.java
+++ src/test/java/de/kiltz/sso/service/SsoServiceTest.java
@@ -1,4 +1,22 @@
 package de.kiltz.sso.service;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SsoServiceTest {
+
+    public final static String DEFAULT_EMAIL = "postfach@tester.de";
+
+    @Autowired
+    SsoService ssoService;
+
+    @Test
+    void IsLoggedTest() {
+        String token = ssoService.createToken(DEFAULT_EMAIL);
+
+    }
+
+
 }
