9b68ef031395421ca4d3e390902182bd01abedba
pandorasinclair <eleonora.zweigardt@gmail.com>
2025-09-19 09:30:11 +0200

    Test Aufgabe2 Token Vergleich

---
+++ src/test/java/de/kiltz/sso/service/TokenAvailabilityTest.java
@@ -0,0 +1,36 @@
+package de.kiltz.sso.service;
+
+import de.kiltz.sso.dao.KontoDao;
+import de.kiltz.sso.data.KontoEntity;
+import de.kiltz.sso.rest.v1.LoginRestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Sql(scripts = "/dataCrud.sql")
+@Transactional
+ class TokenAvailabilityTest {
+    @Autowired
+    private LoginRestService service;
+    @Autowired
+    private KontoDao dao;
+
+    @Test
+    void testTokenAvailableLoginNoLogout(){
+
+        KontoEntity ktest = dao.findByEmailAndPasswort("test2@testa.de", "keins");
+        ResponseEntity<String> firstLoginToken = assertDoesNotThrow(()->service.login(ktest.getEmail(), ktest.getPasswort()));
+        assertEquals(HttpStatus.OK, firstLoginToken.getStatusCode());
+
+        ResponseEntity<String> secondLoginToken = assertDoesNotThrow(()->service.login(ktest.getEmail(), ktest.getPasswort()));
+        assertEquals(HttpStatus.OK, secondLoginToken.getStatusCode());
+        assertEquals(firstLoginToken.getBody(),secondLoginToken.getBody());
+    }
+}
